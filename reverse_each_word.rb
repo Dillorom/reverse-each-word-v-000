9989dea54c96d11c8f51563e746abe9b1924be54
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-01 18:27:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence1 = "Hello there, and how are you?"
 def reverse_each_word(sentence1)
-  sentence1.split(" ")  
+  sentence1.split(" ")
   sentence1.collect do |word|
     word.reverse
   end
