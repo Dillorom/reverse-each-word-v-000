7e9396414ac64070057df676910dc4865e3439c7
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-01 18:46:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 
 def reverse_each_word(string)
   string_to_array = string.split
+  reversed_words = []
+  
   string_to_array.each do |word|
-    word.reverse
-    reversed_words = []
     reversed_words << word.reverse
-  end
-
+    end
+    return reversed_words.join(" ")
 end
