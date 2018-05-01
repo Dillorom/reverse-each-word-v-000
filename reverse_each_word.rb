9805b8d60f6a83f402a7b07107545442c7ad0725
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-01 18:44:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,8 @@ def reverse_each_word(string)
   string_to_array = string.split
   string_to_array.each do |word|
     word.reverse
+    reversed_words = []
+    reversed_words << word.reverse
   end
 
 end
